9d83d74d063202285e57a15d1c318486afa95ca6
rifqi agnia mubarok <rifqiagniamubarok@gmail.com>
2024-02-15 10:12:59 +0700

    initial setup

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3560;
+const { sequelize } = require('./models');
 
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Sequelize checking
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 // routing
 const routes = require('./routes');
 app.use('/', routes);
 
+// Running
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
